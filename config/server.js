721511ffe51c3b37f1538a9fd41f3dc1f2270348
Matias97788 <61601838+Matias97788@users.noreply.github.com>
2021-08-30 16:56:55 -0400

    subida codigo

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9a8dde0cf5ff361fb54351e2829b3b31'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f14c121d-409f-43d2-90b6-783a0ec50cae'
+};
