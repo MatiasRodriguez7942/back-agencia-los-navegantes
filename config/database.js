0f0d6b622478e0b2ef79629a64202b39d64aec7a
Matias97788 <m.rodriguez@agencialosnavegantes.cl>
2021-09-02 16:36:39 -0400

    Update database.js
    Conexion mongodb

--- config/database.js
+++ config/database.js
@@ -2,13 +2,18 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: 'mongoose',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        host: env('DATABASE_HOST', 'cluster0.2iiqs.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'Cluster0'),
+        username: env('DATABASE_USERNAME', 'matias_dev'),
+        password: env('DATABASE_PASSWORD', 'matigol7942'),
       },
       options: {
-        useNullAsDefault: true,
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
       },
     },
   },
